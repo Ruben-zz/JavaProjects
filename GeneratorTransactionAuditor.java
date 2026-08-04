e7c230bcf57fee3efa0f1981e1b02e05fbe3ef2c
Ruben Gil <ruben.university.papers@gmail.com>
2026-08-04 17:36:24 -0400

    V1.4, agregado operador ternario y condicional switch

--- GeneratorTransactionAuditor.java
+++ GeneratorTransactionAuditor.java
@@ -22,9 +22,36 @@ public class GeneratorTransactionAuditor {
 		
 		String email = entrada.nextLine();
 		
+		//Evaluando con condicional switch
+		
+		System.out.println("Ingresa tu sector corporativo");
+		
+		String opciones = (entrada.nextLine().toUpperCase()).strip();
+		
+		String codigoArea = switch(opciones) {
+			case "IT" -> "101";
+			
+			case "FINANZAS" -> "202";
+			
+			case "VENTAS" -> "303";
+		
+			default -> "000";
+		
+		
+		};
+		
+		
+		//Evalua tipo de moneda
+		
+		System.out.println("¿La Transaccion es en dolares?");
+		System.out.println("Si tu respuesta es si escribe true");
+		System.out.println("Si tu respuesta es no escribe false");
+		boolean monedaVerificacion = Boolean.parseBoolean(entrada.nextLine());
+		
+		String simboloMoneda = monedaVerificacion ? "$" : "Bs";
+		
 		
 		//Ingreso de monto y constante IVA
-		
 		System.out.println("Ingresa el monto con almenos 2 decimales: ");
 		double montoBase = Double.parseDouble(entrada.nextLine());
 		//Bucle para verificar si el monto es <= a 0, en dado caso se cierra el programa
@@ -49,12 +76,12 @@ public class GeneratorTransactionAuditor {
 				
 				int numRandom = (int)(((Math.random()*9000)+1000));
 				//calcula el token con numeros random, la longitud de letras del email y la primera letra del nombre
-				String token = (cleanName.charAt(0) + "-" + numRandom + "-" + email.length());
+				String token = (codigoArea + "-" + cleanName.charAt(0) + "-" + numRandom + "-" + email.length());
 				
 				System.out.println("-----------------FACTURA------------");
 				System.out.println("-----Nombre: " + cleanName);
 				System.out.println("-----Correo Electronico: " + email);
-				System.out.println("-----Monto + IVA: " + Math.round(montoFinal) + "$" );
+				System.out.println("-----Monto + IVA: " + Math.round(montoFinal) + simboloMoneda );
 				System.out.println("-----Verificacion: " + verificacion);
 				System.out.println("-----TOKEN SEGURIDAD: " + token);
 			}
